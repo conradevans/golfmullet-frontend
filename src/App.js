6716b6bb42227f3b1763fb05d7f186a1daf1afce
Conrad Evans <conradevans@ufl.edu>
2025-07-18 14:19:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -149,7 +149,7 @@ function App() {
         />
         <Route
           path="/account"
-          element={<AccountPage setIsLoggedIn={setIsLoggedIn} />}
+          element={<AccountPage setIsLoggedIn={setIsLoggedIn} logoutUser={logoutUser} />}
         />
         <Route path="/browse" element={<Browse clothes={clothes} />} />
         <Route
